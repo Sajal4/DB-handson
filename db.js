7e7678607dc23e221e1cef1339e492989c337fc6
Sajal4 <sajaldebnath923@gmail.com>
2022-08-02 18:36:12 +0530

    done

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const employeeArr = require("./employee.json");
 const mongoClient = require('mongodb').MongoClient;
 
-const connectionString = "mongodb://127.0.0.1:27017/mongo";
+const connectionString = "mongodb://127.0.0.1:27017/";
 
 mongoClient.connect(connectionString, async (err, db) => {
     if(err) {
